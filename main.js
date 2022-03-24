ea470fc713cfdc172cc62ec5540e45c0eca9fb8e
EliusHHimel <himelahmed201617@gmail.com>
2022-03-24 13:27:32 +0000

    dom event multiple element problem solved

--- main.js
+++ main.js
@@ -8,21 +8,25 @@ ratingContainer.addEventListener('click', (e) =>{
 
 submitBtn.addEventListener('click', ()=>{
 thanksModal.style.display = 'block';
+console.log(rating.length)
 thanksModal.innerHTML = `
 <center>
 <img src="images/illustration-thank-you.svg" alt="">
 <p class="selected-rating">
-You selected ${rating ?  rating : 'nothing'} out of 5
+You selected ${rating && rating.length < 2 ?  rating : 'nothing'} out of 5
 </p>
-<h2>${rating ? 'Thank you!' : ''}</h2>
+<h2>${rating && rating.length < 2 ? 'Thank you!' : ''}</h2>
 <p>
-${rating ? "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!" : 'Please go back and select a rating.'}
+${rating && rating.length < 2 ? "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!" : 'Please go back and select a rating.'}
 </p>
-${rating ? '' : "<button class='back-btn' id='back-btn'>Back</button>"}
+${rating && rating.length < 2 ? '' : "<button class='back-btn' id='back-btn'>Back</button>"}
 </center>
 `
-const backBtn = document.getElementById('back-btn');
-backBtn.addEventListener('click', ()=>{
-    thanksModal.style.display = 'none'
-})
+if(!rating || rating.length == 0 || rating.length > 1){
+    const backBtn = document.getElementById('back-btn');
+    backBtn.addEventListener('click', ()=>{
+        thanksModal.style.display = 'none'
+    })
+}
+
 })
